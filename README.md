f85bf819b2dd05bf700017dfa5315999e2cb4469
Feenert <tristanfeener@gmail.com>
2022-02-19 14:06:44 -0500

    Added simple GUI, reworked Javascript

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rock-Paper-Scissors
+
+Art by:
+Designed by vectorstock (Image #25169737 at VectorStock.com)
+https://www.vectorstock.com/royalty-free-vector/rock-paper-scissors-hand-gesture-vector-25169737
--- script.js
+++ script.js
@@ -1,94 +1,69 @@
-// 0 = rock|| 1= paper|| 2 = scissors
 
-function choiceConverter(choice){
-    if (choice == 0 || choice == "rock"){
-        return "rock";
-    } else if (choice == 1 || choice == "paper"){
-        return "paper"
-    } else if (choice == 2 || choice == "scissors"){
-        return "scissors"
-    } else {
-        return "Incorret input";
-    }
-}
-
-function computerPlay(){
-    let computerNumChoice = Math.floor(Math.random()*3);
-    let computerChoice = choiceConverter(computerNumChoice);
-    return computerChoice;
-}
-
-function playerChoice(){
-    let play = prompt("Rock, Paper, or Scissors?")
-    play.toLowerCase();
-    playerSelection = choiceConverter(play);
-    return playerSelection;
-}
-
-function compare(){
-   let computerSelection = computerPlay();
-   console.log(computerSelection);
-   let playerSelection =  playerChoice();
-   console.log(playerSelection);
-
-   if (playerSelection == "rock" && computerSelection == "paper"){
-       return "Computer wins!";
-   } else if (playerSelection == "rock" && computerSelection == "scissors"){
-       return "Player wins!";
-   } else if (playerSelection == "paper" && computerSelection == "scissors"){
-       return "Computer wins!";
-   } else if (playerSelection == "paper" && computerSelection == "rock"){
-       return "Player wins!";
-   } else if (playerSelection == "scissors" && computerSelection == "rock"){
-       return "Computer wins!";
-   } else if (playerSelection == "scissors" && computerSelection == "paper"){
-       return "Player wins!";
-   } else if(playerSelection == "Incorrect input"){
-       return false
-   } else {
-        return "Draw.";
-   }
-}
+// rock = 0 || paper = 1 || scissors = 2 ||
 
 let playerScore = 0;
 let computerScore = 0;
+let roundOver = playerScore + computerScore;
+const images = [];
+images[0] = "./images/rock.png";
+images[1] = "./images/paper.png";
+images[2] = "./images/scissors.png";
 
-function game(){
-    let i = 0;
+function roundImages(imageIndex){
+    return `<img src="${images[imageIndex]}"></img>`
+};
 
-    while (i <= 4){
-    let winnerRound = compare();
+function game(playerChoice){
+let computerChoice = Math.floor(Math.random()*3);
 
-    if (winnerRound == false){
-        break
-    } else{
-        if (winnerRound == "Computer wins!"){
-            computerScore = computerScore + 1;
-            console.log(computerScore);
-            if (computerScore == 3){
-                return alert("Computer wins!")
-            }
-        }
-
-        if (winnerRound == "Player wins!"){
-            playerScore = playerScore + 1;
-            console.log(playerScore);
-            if (playerScore == 3){
-                return alert("Player wins!")
-            }
-        }
-
-        if (winnerRound == "Draw."){
-            alert("Draw");
-        } else {
-            let again = prompt(winnerRound + " Player Score: " + playerScore + " Computer Score: " + computerScore + " Next game (y/n)");
-            if (again == "y"){
-                i = i +1
-                console.log(i);
-        }   else if (again == "n") {
-            return
-        }
-        }
+    if(playerChoice == computerChoice){
+        console.log('draw')
+    } else if( playerChoice == 0 && computerChoice == 1){
+        console.log('bot wins');
+        computerScore++;
+        document.getElementById('computerScore').outerHTML = `<h1 id="computerScore">Computer: ${computerScore} </h1>`;
+    } else if (playerChoice == 0 && computerChoice == 2){
+        console.log('you win');
+        playerScore++;
+        document.getElementById('playerScore').outerHTML = `<h1 id="playerScore">Player: ${playerScore} </h1>`;
+    } else if (playerChoice == 1 && computerChoice == 0){
+        console.log('you win');
+        playerScore++;
+        document.getElementById('playerScore').outerHTML = `<h1 id="playerScore">Player: ${playerScore} </h1>`;
+    } else if(playerChoice == 1 && computerChoice == 2) {
+        console.log('you lose');
+        computerScore++;
+        document.getElementById('computerScore').outerHTML = `<h1 id="computerScore">Computer: ${computerScore} </h1>`;
+    } else if(playerChoice == 2 && computerChoice == 0) {
+        console.log('you lose');
+        computerScore++;
+        document.getElementById('computerScore').outerHTML = `<h1 id="computerScore">Computer: ${computerScore} </h1>`;
+    } else if(playerChoice == 2 && computerChoice == 1){
+        console.log('you win');
+        playerScore++;
+        document.getElementById('playerScore').outerHTML = `<h1 id="playerScore">Player: ${playerScore} </h1>`;
     }
+
+    document.getElementById('playerImage').innerHTML = roundImages(playerChoice);
+    document.getElementById('computerImage').innerHTML = roundImages(computerChoice);
+    let roundOver = playerScore + computerScore;
+
+    if(roundOver == 5 || playerScore == 3 || computerScore == 3){
+        document.getElementById('gameOver').innerHTML = "Game Over";
+        let playAgain = document.createElement('button');
+        playAgain.innerHTML = "Play Again";
+        document.body.appendChild(playAgain);
+        playAgain.addEventListener("click",() =>{
+            playerScore = 0;
+            computerScore = 0;
+            document.getElementById('computerScore').outerHTML = `<h1 id="computerScore">Computer: ${computerScore} </h1>`;
+            document.getElementById('playerScore').outerHTML = `<h1 id="playerScore">Player: ${playerScore} </h1>`;
+            document.getElementById('gameOver').innerHTML = "";
+            playAgain.remove();
+        });
+       
     }
 }
+
+// document.getElementById('playerScore').outerHTML = `<h1 id="playerScore">Player: ${playerScore} </h1>`
+// document.getElementById('computerScore').outerHTML = `<h1 id="computerScore">Computer: ${computerScore} </h1>`
